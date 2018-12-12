26ca9521c928fb14f75b3cd25dc0709a4a6e4261
David Villacis <david.villacis02|epn.edu.ec>
2018-12-11 19:12:33 -0500

    started on pd gap for generic l1l2 solver

--- lower_level_solver/misc/compute_generic_l1_l2_pd_gap.m
+++ lower_level_solver/misc/compute_generic_l1_l2_pd_gap.m
@@ -1,10 +1,14 @@
-function [gap] = compute_generic_l1_l2_pd_gap(x,y,Ks,Bs,)
+function [gap] = compute_generic_l1_l2_pd_gap(x,y,Ks,Bs,lambda,alpha,z,q)
 %COMPUTE_GENERIC_L1_L2_PD_GAP Calculate the primal-dual gap for the
 %generic_l1_l2 solver.
 %   Detailed explanation goes here
 
 % Calculate primal value
+primal_1 = sum(lambda.*norm(Ks*x-z).^2);
+primal_2 = sum(alpha.*norm(Bs*x-q,1));
+primal = primal_1 + primal_2;
 
+% Calculate dual value
 
+gap = primal;
 end
-
