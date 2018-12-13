5eaa4540af0c6b08b5329985921c34d1322e8d89
David Villacis <david.villacis02|epn.edu.ec>
2018-12-13 11:37:52 -0500

    Fixed pd gap for chambolle pock

--- lower_level_solver/misc/compute_rof_pd_gap.m
+++ lower_level_solver/misc/compute_rof_pd_gap.m
@@ -3,13 +3,13 @@ function [gap, primal, dual] = compute_rof_pd_gap(nabla, u, p, f, lambda, epsilo
   div_p = nabla'*p;
   nabla_u = nabla*u;
 
-  nu = sqrt(sum(reshape(nabla_u, M*N, 2).^2,2));
+  nu = rssq(reshape(nabla_u, M*N, 2),2);
   idx = nu < epsilon;
-  huber = nu-epsilon/2;
-  huber(idx) = nu(idx).^2/2/epsilon;
+  huber = nu - 0.5*epsilon;
+  huber(idx) = (0.5/epsilon) * nu(idx).^2;
 
-  primal = lambda * sum(huber) + 1/2*norm(u-f)^2;
-  dual = -norm(div_p)^2/2 + f'*div_p - epsilon/lambda/2*norm(p)^2;
+  primal = lambda * sum(huber) + 0.5*sumsqr(u-f);
+  dual = -0.5*sumsqr(div_p) + f'*div_p - epsilon/(2*lambda*sumsqr(p));
 
   gap = primal-dual;
 end
