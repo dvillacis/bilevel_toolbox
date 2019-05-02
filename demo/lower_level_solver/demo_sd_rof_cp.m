a29521163e64f53376d64b088c70f9c522d4f879
David Villacis <david.villacis02|epn.edu.ec>
2019-05-02 11:55:51 -0500

    Better plotting in sd examples

--- demo/lower_level_solver/demo_sd_rof_cp.m
+++ demo/lower_level_solver/demo_sd_rof_cp.m
@@ -16,3 +16,8 @@ param.alpha = 0.1*triu(ones(M,N))+10.5*tril(ones(M,N));
 
 %% Solving
 [denoised,gap] = solve_sd_rof_cp_single_gaussian(noisy,param);
+
+%% Plotting
+imagesc_gray(dataset.get_target(1),1,'Original','131');
+imagesc_gray(noisy,1,'Gaussian Noise','132');
+imagesc_gray(denoised,1,'SD-ROF Image Denoising','133');
--- demo/lower_level_solver/demo_sd_rof_generic.m
+++ demo/lower_level_solver/demo_sd_rof_generic.m
@@ -22,3 +22,8 @@ lambda = ones(M,N);
 
 %% Solving
 [denoised,gap] = solve_generic_l1_l2({lambda},{alpha},{id_op},{grad_op},noisy,q,0,0*noisy,param);
+
+%% Plotting
+imagesc_gray(dataset.get_target(1),1,'Original','131');
+imagesc_gray(noisy,1,'Gaussian Noise','132');
+imagesc_gray(denoised,1,'SD-ROF Image Denoising','133');
